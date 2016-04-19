0ba23ecd801111d3ccc19697d19ba1af729b99ac
Kendall Spackman <Kendall Spackman>
2016-04-19 17:40:16 -0600

    Add comments

--- public/routes/high-scores.js
+++ public/routes/high-scores.js
@@ -1,3 +1,7 @@
+// This file is based on sample code by Dean Mathias 
+// these samples are used by permission and were distributed for 
+// USU CS5410 Game Development Spring 2016
+// Code modifications by Austin Stoker and Kendall Spackman
 
 var scores = [0, 0, 0, 0, 0];
 
